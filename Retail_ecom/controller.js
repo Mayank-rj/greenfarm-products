fa3647d32a15b6e6125b8765a33914597fe31357
Mayank-rj <mayank.kumar.rajoria@gmail.com>
2026-07-19 15:34:42 +0530

    ecom and crm

--- Retail_ecom/controller.js
+++ Retail_ecom/controller.js
@@ -1,7 +1,7 @@
 const SITE_CONFIG = {
   // apiIP: 'http://13.203.57.229/api/retail', //production
   // apiIP: 'https://retailpos.greenfarmproducts.com.au/api/retail', //production,
-  apiIP: "http://13.201.57.251/api/retail", //testing
+  apiIP: "https://greenfarm-products-backend.onrender.com/api/retail", //testing
   // apiIP: "http://localhost:5011/api/retail", //testing
   apiIPMongo: "https://retailpos.greenfarmproducts.com.au/",
   imageUrl: "https://retailpos.greenfarmproducts.com.au/images/",
